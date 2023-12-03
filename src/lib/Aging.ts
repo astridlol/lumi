74c740d7d01001e673696800c45a475d879c25f3
astridlol <sites@astrid.sh>
2023-12-03 11:18:57 -0500

    refactor(Aging): extract embed creation

--- src/lib/Aging.ts
+++ src/lib/Aging.ts
@@ -2,12 +2,32 @@ import { client, prisma } from '..';
 import * as cron from 'node-cron';
 import * as Embeds from '../constants/Embeds';
 import { ButtonBuilder } from '@discordjs/builders';
-import { ActionRowBuilder, ButtonStyle } from 'discord.js';
+import { ActionRowBuilder, ButtonStyle, MessageCreateOptions } from 'discord.js';
 import * as LumiUtils from '../lib/Lumi';
+import { Lumi } from '@prisma/client';
 
 cron.schedule('*/15 * * * *', async () => babyAging());
 
 babyAging();
+
+const getBdayEmbed = (lumi: Lumi): MessageCreateOptions => {
+	const embed = Embeds.success()
+		.setTitle(`Happy birthday ${lumi.name}!`)
+		.setDescription(`${lumi.name} is now ${lumi.age}! Wish them a happy birthday below.`);
+	const wish = new ButtonBuilder()
+		.setCustomId('praise')
+		.setStyle(ButtonStyle.Success)
+		.setLabel('Wish Happy Birthday')
+		.setEmoji({
+			name: '🎂'
+		});
+	const row = new ActionRowBuilder<ButtonBuilder>().addComponents(wish);
+
+	return {
+		embeds: [embed],
+		components: [row]
+	};
+};
 
 async function babyAging() {
 	const allBabyLumi = await prisma.lumi.findMany({
@@ -19,26 +39,11 @@ async function babyAging() {
 	});
 
 	allBabyLumi.forEach(async (f) => {
-		const embed = Embeds.success()
-			.setTitle(`Happy birthday ${f.name}!`)
-			.setDescription(`${f.name} is now ${f.age}! Wish them a happy birthday below.`);
-		const wish = new ButtonBuilder()
-			.setCustomId('praise')
-			.setStyle(ButtonStyle.Success)
-			.setLabel('Wish Happy Birthday')
-			.setEmoji({
-				name: '🎂'
-			});
-		const row = new ActionRowBuilder<ButtonBuilder>().addComponents(wish);
-
 		LumiUtils.modifyCoins(f.playerId, 2, 'increment');
 
 		const user = await client.users.fetch(f.playerId);
 		try {
-			await user.send({
-				embeds: [embed],
-				components: [row]
-			});
+			await user.send(getBdayEmbed(f));
 		} catch (err) {
 			console.log(err);
 			// If the DM failed to send, the user has probably blocked Lumi. Decrement 20 happiness points, as that's just harsh.
